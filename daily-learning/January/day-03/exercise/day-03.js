b68d151a8f15cd3c0007e9a9779aa28d552a64ec
ErDprogrammer <rickemzy5@gmail.com>
2025-01-03 23:36:36 +0100

    day- 03 commit

---
+++ daily-learning/January/day-03/exercise/day-03.js
@@ -0,0 +1,4 @@
+let message = 'JavaScript is lightweight! ';
+ 
+const Username = 'Richard';
+console.log('Hello, ' + Username + '! just wanted to let you know that ' + message  );
---
+++ daily-learning/January/day-03/exercise/day-03Lecture.js
@@ -0,0 +1,6 @@
+ function intro (Username){
+    const header = "welcome to coding javascript with me. i am " + Username + " your host";
+    console.log(header);
+    
+}
+intro("Richard");
