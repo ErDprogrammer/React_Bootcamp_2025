ba4111551ebe4d39403ab6a5b9f31d51bb827bdd
ErDprogrammer <rickemzy5@gmail.com>
2025-01-05 17:32:33 +0100

    day -04 exercise

--- daily-learning/January/day-03/exercise/day-03Lecture.js
+++ daily-learning/January/day-03/exercise/day-03Lecture.js
@@ -1,6 +1,5 @@
  function intro (Username){
     const header = "welcome to coding javascript with me. i am " + Username + " your host";
     console.log(header);
-    
 }
 intro("Richard");
---
+++ daily-learning/January/day-04/exercise/day-04.js
@@ -0,0 +1,1 @@
+const scriptingLanguage = "javascript is a scripting language created as easy to use to help static html page interactive, is a scripting language because you can execute codes on the console in the dev part of the browser";
