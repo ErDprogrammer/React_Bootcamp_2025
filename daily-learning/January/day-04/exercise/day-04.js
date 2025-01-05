9ce323a763db283d76c3f0071b413d70e2b1320e
ErDprogrammer <rickemzy5@gmail.com>
2025-01-05 17:41:08 +0100

    day -04 exercise

--- daily-learning/January/day-04/exercise/day-04.js
+++ daily-learning/January/day-04/exercise/day-04.js
@@ -1,3 +1,3 @@
-const scriptingLanguage = "javascript is an example of scripting language created as easy to use to help static html page interactive, is a scripting language because you can execute codes on the console in the dev part of the browser";
+const scriptingLanguage = "javascript is an example of scripting language created and is easy to use, it was created to help static html page interactive, it's a scripting language because you can execute codes on the console in the dev part of the browser";
 
 console.log(scriptingLanguage);
