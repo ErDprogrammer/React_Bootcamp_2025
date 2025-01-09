14b9e3d22732c66777f63c8191f00fe2fc5ca5c0
Henry Richard <83845507+ErDprogrammer@users.noreply.github.com>
2025-01-09 10:01:32 +0100

    Update day-08.js

--- daily-learning/January/day-08/exercise/day-08.js
+++ daily-learning/January/day-08/exercise/day-08.js
@@ -1,3 +1,47 @@
-'use strict';
-const header = "we would be on strict mode from now on";
-console.log(header);
+// String variables
+const string1 = "Hello";
+const string2 = "World";
+const string3 = "JavaScript";
+const string4 = "Coding";
+const string5 = "Variable";
+const string6 = "Data Type";
+const string7 = "String Example";
+const string8 = "Learning";
+const string9 = "Development";
+const string10 = "Task";
+
+// Number variables
+const number1 = 10;
+const number2 = 25.5;
+const number3 = 100;
+const number4 = -50;
+const number5 = 0;
+const number6 = 99999;
+const number7 = 3.14;
+const number8 = 2025;
+const number9 = -1;
+const number10 = 42;
+
+// Boolean variables
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = true;
+const boolean4 = false;
+const boolean5 = true;
+const boolean6 = false;
+const boolean7 = true;
+const boolean8 = false;
+const boolean9 = true;
+const boolean10 = false;
+
+// Undefined variables
+let undefined1;
+let undefined2;
+let undefined3;
+let undefined4;
+let undefined5;
+let undefined6;
+let undefined7;
+let undefined8;
+let undefined9;
+let undefined10;
