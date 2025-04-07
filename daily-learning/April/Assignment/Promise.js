def8177bde059edcfea896fff88b780410fdc584
ErDprogrammer <rickemzy5@gmail.com>
2025-04-07 22:51:13 +0100

    this is the promise

---
+++ daily-learning/April/Assignment/Promise.js
@@ -0,0 +1,21 @@
+function confirmPayment(balance) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (balance >= 100) {
+          resolve("Payment successful ✅");
+        } else {
+          reject("Insufficient balance ❌");
+        }
+      }, 1000);  
+    });
+  }
+  
+  // Usage
+  confirmPayment(120)
+    .then((message) => {
+      console.log("Success:", message);
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+  
